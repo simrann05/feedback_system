8a928f53251cf969a38f0478a3f4e3a1ef4595b4
simrann05 <165164400+simrann05@users.noreply.github.com>
2025-07-22 09:38:41 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	ob_start(); //Turns on output buffering 
 
 	$timezone = date_default_timezone_set("Europe/London");
-	$conn = mysqli_connect("localhost", "root", "simm", "feedback"); //Connection variable
+	$conn = mysqli_connect("localhost", "root", " ", "feedback"); //Connection variable
 
 	if($conn) {
 		echo "Successful " ;
